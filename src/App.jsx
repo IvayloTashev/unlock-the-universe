7f62667ba5f4a4516f2f8de7a690a13fa8503a8e
IvayloTashev <i.tashev@abv.bg>
2024-07-14 12:53:28 +0300

    Adding tailwind and sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import './App.css'
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
 import Navbar from "./components/navbar/Navbar";
-import { Route, Routes } from "react-router-dom";
-import './App.css'
+import SignUp from "./components/sign-up/SignUp";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
 
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/sign-up" element={<SignUp />} />
             </Routes>
         </>
     )
--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -16,7 +16,7 @@ export default function Button({
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
     return (
-        <Link to="/sing-up" className='btn-mobile'>
+        <Link to="/sign-up" className='btn-mobile'>
             <button
                 className={`btn ${checkButtonStyle} ${checkButtonSize}`}
                 onClick={onClick}
