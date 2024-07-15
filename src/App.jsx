cefec52015f60ce71d25d20e6c5bd90234603be5
IvayloTashev <i.tashev@abv.bg>
2024-07-15 20:45:46 +0300

    Added register page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import SignUp from "./components/sign-up/SignUp";
+import Register from './components/sign-up/register/Register';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </>
     )
--- src/components/sign-up/register/Register.jsx
+++ src/components/sign-up/register/Register.jsx
@@ -1,4 +1,4 @@
-export default function SignUp() {
+export default function Register() {
     return (
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -9,7 +9,7 @@ export default function SignUp() {
                         className="mx-auto h-10 w-auto"
                     /> */}
                     <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
-                        Sign in to your account
+                        Create your account
                     </h2>
                 </div>
 
@@ -55,19 +55,42 @@ export default function SignUp() {
                         </div>
 
                         <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="rePass" className="block text-lg font-medium leading-6 text-white">
+                                    Repeat Password
+                                </label>
+                                <div className="text-sm">
+                                    {/* <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                      Forgot password?
+                    </a> */}
+                                </div>
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="rePass"
+                                    name="rePass"
+                                    type="rePass"
+                                    required
+                                    autoComplete="current-password"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
                             <button
                                 type="submit"
                                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                             >
-                                Sign in
+                                Register
                             </button>
                         </div>
                     </form>
 
                     <p className="mt-10 text-center text-white text-lg">
-                        Don't have an account?{' '}
-                        <a href="/register" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 text-xl">
-                            Create an account
+                        Already have an account?{' '}
+                        <a href="/sign-up" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500 text-xl">
+                            Sing In
                         </a>
                     </p>
 
