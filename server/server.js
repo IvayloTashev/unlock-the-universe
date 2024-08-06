500f10470eef1776aa0f1704af51aae047091e14
IvayloTashev <i.tashev@abv.bg>
2024-08-06 21:24:21 +0300

    Add data to server

--- server/server.js
+++ server/server.js
@@ -1340,6 +1340,44 @@
         }
     };
     var seedData = {
+        celestialBodies: {
+            "343a7755-a1c0-42de-95a1-154c79f6c9f9": {
+                "_ownerId": "60f0cf0b-34b0-4abd-9769-8c42f830dffc",
+                "title": "Stars",
+                "image": "https://science.nasa.gov/wp-content/uploads/2023/06/sun-h1180-jpg.webp?w=1920",
+                "urlTitle": "stars",
+                "description": "Stars are giant balls of hot gas - mostly hydrogen, with some helium and small amounts of other elements. Every star has its own life cycle, ranging from a few million to trillions of years, and its properties change as it ages.",
+                "_createdOn": 1722966828433,
+                "_id": "343a7755-a1c0-42de-95a1-154c79f6c9f9"
+            },
+            "356e4a2c-b4a9-4f9e-8ace-7a5f1b561471": {
+                "_ownerId": "60f0cf0b-34b0-4abd-9769-8c42f830dffc",
+                "title": "Planets",
+                "image": "https://img.freepik.com/free-photo/planets-solar-system_23-2150042459.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1722729600&semt=ais_hybrid",
+                "urlTitle": "planets",
+                "description": "A planet is a celestial body that is in orbit around the star, has sufficient mass for its self-gravity to overcome rigid body forces so that it assumes a hydrostatic equilibrium shape, and has cleared the neighborhood around its orbit.",
+                "_createdOn": 1722966874744,
+                "_id": "356e4a2c-b4a9-4f9e-8ace-7a5f1b561471"
+            },
+            "95849163-74b0-4256-976d-7f4b0428647f": {
+                "_ownerId": "60f0cf0b-34b0-4abd-9769-8c42f830dffc",
+                "title": "Galaxies",
+                "image": "https://cdn.theatlantic.com/thumbor/cOJT3NgUpm_xigNqb7pz7yEGV9Q=/0x0:2133x1200/960x540/media/img/mt/2022/08/noirlab2219a/original.jpg",
+                "urlTitle": "galaxies",
+                "description": "Galaxies consist of stars, planets, and vast clouds of gas and dust, all bound together by gravity. The largest contain trillions of stars and can be more than a million light-years across. The smallest can contain a few thousand stars and span just a few hundred light-years.",
+                "_createdOn": 1722966901910,
+                "_id": "95849163-74b0-4256-976d-7f4b0428647f"
+            },
+            "1f86ccea-e7f8-45c2-b089-8c625abd100c": {
+                "_ownerId": "60f0cf0b-34b0-4abd-9769-8c42f830dffc",
+                "title": "Black Holes",
+                "image": "https://cdn.mos.cms.futurecdn.net/HsDtpFEHbDpae6wBuW5wQo.jpg",
+                "urlTitle": "blackHoles",
+                "description": "A black hole is an astronomical object with a gravitational pull so strong that nothing, not even light, can escape it. A black hole's “surface,” called its event horizon, defines the boundary where the velocity needed to escape exceeds the speed of light, which is the speed limit of the cosmos.",
+                "_createdOn": 1722966921481,
+                "_id": "1f86ccea-e7f8-45c2-b089-8c625abd100c"
+            }
+        },
         blackHoles: {
             "5322ab7e-2f2c-46cb-a695-9fcc90a2b1f4": {
                 "title": "Sagittarius A*",
